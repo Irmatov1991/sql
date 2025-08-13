7dcb51ed380380f4696f6e3b5bf945175c249a6f
Irmatov1991 <irmatovmiralisher@gmail.com>
2025-08-13 10:19:45 +0500

    Add files via upload

---
+++ SQLQuery12.sql
@@ -0,0 +1,18 @@
+create database class6	
+
+create table students (id int, name varchar(30), group_name varchar(30),age int)
+
+select * from students
+
+--declare @son int = 32769
+--select @son
+
+--declare @son flat = 32769
+--select @son
+
+--declare @son decinal(5,2) = 123,78
+--select @son
+
+--declare @son date(5,2) = 123,78
+--select @son
+
